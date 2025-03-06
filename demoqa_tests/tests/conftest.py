4da4ff6995c90c045e87e6e93124ee507eea31fb
Ilia <kalinin499studing@gmail.com>
2025-03-06 17:32:28 +0300

    add test for registration user

---
+++ demoqa_tests/tests/conftest.py
@@ -0,0 +1,50 @@
+import os
+import pytest
+from selene import browser
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from demoqa_tests.utils import attach
+from dotenv import load_dotenv
+
+
+@pytest.fixture(scope="session")
+def setup_env():
+    load_dotenv()
+
+
+@pytest.fixture(scope='function')
+def browser_manager(setup_env):
+    browser.config.base_url = 'https://demoqa.com'
+    browser.config.window_height = 1080
+    browser.config.window_width = 1920
+
+    options = Options()
+    selenoid_capabilities = {
+        "browserName": "chrome",
+        "browserVersion": "133.0",
+        "selenoid:options": {
+            "enableVNC": True,
+            "enableVideo": True
+        }
+    }
+
+    SELENOID_LOGIN = os.getenv("SELENOID_LOGIN")
+    SELENOID_PASSWORD = os.getenv("SELENOID_PASSWORD")
+    SELENOID_URL = os.getenv("SELENOID_URL")
+
+    options.capabilities.update(selenoid_capabilities)
+    driver = webdriver.Remote(
+        command_executor=f"https://{SELENOID_LOGIN}:{SELENOID_PASSWORD}@{SELENOID_URL}/wd/hub",
+        options=options
+    )
+
+    browser.config.driver = driver
+
+    yield browser
+
+    attach.add_screenshot(browser)
+    attach.add_logs(browser)
+    attach.add_html(browser)
+    attach.add_video(browser)
+
+    browser.quit()
