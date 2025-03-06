534ccc93f0917ca2975fd87bb3fc6fe5f54a1aff
Ilia <kalinin499studing@gmail.com>
2025-03-06 17:53:48 +0300

    change url in conftest

--- demoqa_tests/tests/conftest.py
+++ demoqa_tests/tests/conftest.py
@@ -9,7 +9,7 @@ from dotenv import load_dotenv
 
 @pytest.fixture(scope='function', autouse=True)
 def setup_browser():
-    browser.config.base_url = 'https://demoqa.com/automation-practice-form'
+    browser.config.base_url = 'https://demoqa.com'
     driver_options = webdriver.ChromeOptions()
     driver_options.page_load_strategy = 'eager'
     browser.config.driver_options = driver_options  # чтоб тест выполнялся когда сайт продолжается грузиться , но html загрузился
