6109034e1898bca27e817d6833b7883c9621bcac
Ilia <kalinin499studing@gmail.com>
2025-03-06 18:30:48 +0300

    change conftest

--- demoqa_tests/tests/conftest.py
+++ demoqa_tests/tests/conftest.py
@@ -12,11 +12,16 @@ def setup_env():
     load_dotenv()
 
 
-@pytest.fixture(scope='function')
-def browser_manager(setup_env):
+@pytest.fixture(scope='function', autouse=True)
+def setup_browser():
     browser.config.base_url = 'https://demoqa.com'
-    browser.config.window_height = 1080
+    driver_options = webdriver.ChromeOptions()
+    driver_options.page_load_strategy = 'eager'
+    browser.config.driver_options = driver_options  # чтоб тест выполнялся когда сайт продолжается грузиться , но html загрузился
     browser.config.window_width = 1920
+    browser.config.window_height = 1080
+    # driver_options = webdriver.ChromeOptions()   #настройка чтоб не открывать браузер , надо для этого 8 , 10 строчку кода
+    # driver_options.add_argument('--headless')
 
     options = Options()
     selenoid_capabilities = {
