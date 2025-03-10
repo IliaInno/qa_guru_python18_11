bef2968bce9dedcd807af78f734e69634f0e3543
Ilia <kalinin499studing@gmail.com>
2025-03-10 13:49:55 +0300

    change conftest

--- demoqa_tests/tests/conftest.py
+++ demoqa_tests/tests/conftest.py
@@ -1,5 +1,4 @@
-import os
-import pytest
+import pytest, os
 from selene import browser
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
@@ -17,16 +16,14 @@ def setup_browser():
     browser.config.base_url = 'https://demoqa.com'
     driver_options = webdriver.ChromeOptions()
     driver_options.page_load_strategy = 'eager'
-    browser.config.driver_options = driver_options  # чтоб тест выполнялся когда сайт продолжается грузиться , но html загрузился
+    browser.config.driver_options = driver_options
     browser.config.window_width = 1920
     browser.config.window_height = 1080
-    # driver_options = webdriver.ChromeOptions()   #настройка чтоб не открывать браузер , надо для этого 8 , 10 строчку кода
-    # driver_options.add_argument('--headless')
 
     options = Options()
     selenoid_capabilities = {
         "browserName": "chrome",
-        "browserVersion": "125.0",
+        "browserVersion": "100.0",
         "selenoid:options": {
             "enableVNC": True,
             "enableVideo": True
@@ -34,8 +31,12 @@ def setup_browser():
     }
 
     options.capabilities.update(selenoid_capabilities)
+
+    login = os.getenv('LOGIN')
+    password = os.getenv('PASSWORD')
+
     browser.config.driver = webdriver.Remote(
-        command_executor=f"https://user1:1234@selenoid.autotests.cloud/wd/hub",
+        command_executor=f"https://{login}:{password}@selenoid.autotests.cloud/wd/hub",
         options=options)
 
     yield browser
